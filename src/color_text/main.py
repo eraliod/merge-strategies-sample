df056e50d1feadb43b6e26840fface71626ec39d
eraliod <22479428+eraliod@users.noreply.github.com>
2024-04-24 10:49:49 -0400

    added a comment in an area to create a conflict

--- src/color_text/main.py
+++ src/color_text/main.py
@@ -15,6 +15,7 @@ def print_boxed_text(text, color):
     print(colored(text_line, color))
     print(colored(top_bottom_border, color))
     pass
+    # some comment
 
 def main():
     parser = create_parser()
